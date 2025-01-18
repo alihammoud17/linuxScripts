023497b6a3a7694f4d3d6fafed41aee1c25f44bc
ahammoud <ahammoud@PORT2024090201>
2025-01-18 11:30:00 +0200

    push files jenkins 2

--- Install_Jenkins.bash
+++ Install_Jenkins.bash
@@ -7,10 +7,10 @@ echo "Updating package list..."
 sudo apt update -y
 
 echo "Adding Jenkins GPG key..."
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo "Adding Jenkins repository..."
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 echo "Updating package list after adding Jenkins repository..."
 sudo apt update -y
