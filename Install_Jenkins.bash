5c3d3144e2783a3d545a6dbd06101f3d47e74917
ahammoud <ahammoud@PORT2024090201>
2025-01-18 11:10:21 +0200

    push files jenkins

--- Install_Jenkins.bash
+++ Install_Jenkins.bash
@@ -7,7 +7,7 @@ echo "Updating package list..."
 sudo apt update -y
 
 echo "Adding Jenkins GPG key..."
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins-keyring.asc
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo "Adding Jenkins repository..."
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
